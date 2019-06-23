a57bae280285a4dc328788abb8474170b7229ba2
Bruno Arruda <arrudabruno10@gmail.com>
2019-06-23 11:07:09 -0300

    Added generic methods for file operations
    FileController now is the main source to get folder paths
    Added also shortcuts methods to save user data and keys easily.

--- src/main/java/com/brunoarruda/hyper_dcpabe/io/FileController.java
+++ src/main/java/com/brunoarruda/hyper_dcpabe/io/FileController.java
@@ -2,10 +2,17 @@ package com.brunoarruda.hyper_dcpabe.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import com.brunoarruda.hyper_dcpabe.User;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+
+import sg.edu.ntu.sce.sands.crypto.dcpabe.key.PublicKey;
+import sg.edu.ntu.sce.sands.crypto.utility.Utility;
 
 /**
  * FileController
@@ -15,46 +22,91 @@ public final class FileController {
     private static final FileController INSTANCE = new FileController();
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    private File dataFolder;
+    private String dataFolder;
 
-    private FileController() {}
+    private FileController() {
+    }
 
     public static FileController getInstance() {
         return INSTANCE;
     }
 
-    public void configure(String path) {
-        dataFolder = new File(path);
-        if (!dataFolder.exists() || !dataFolder.isDirectory()) {
-            dataFolder.mkdirs();
+    public FileController configure(String path) {
+        this.dataFolder = path;
+        File dirFile = new File(path);
+        if (!dirFile.exists() || !dirFile.isDirectory()) {
+            dirFile.mkdirs();
         }
-
         // to enable standard indentation ("pretty-printing"):
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
         // to allow serialization of "empty" POJOs (no properties to serialize)
         mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        return this;
     }
 
-    public File getDataDirectory(String path) {
-        return dataFolder;
+    public String getDataDirectory() {
+        return dataFolder + "\\";
     }
 
-    public void writeUser(String directoryPath, User user) {
-        if (dataFolder == null) {
-            configure(directoryPath);
+    public String getUserDirectory(User user) {
+        return getUserDirectory(user, "");
+    }
+
+    public String getUserDirectory(User user, String subDirectory) {
+        String postfix = "";
+        if (!subDirectory.equals("")) {
+            postfix = subDirectory + "\\";
         }
-        String dir = String.format("%s%s%s-%s", dataFolder.getPath(), File.separator, user.getName(),
+        String dir = String.format("%s%s-%s", getDataDirectory(), user.getName(),
                 user.getPublicECKey().substring(0, 8));
-        File userDir = new File(dir);
-        userDir.mkdir();
+        return dir + "\\" + postfix;
+    }
+
+    public <T> void writeToDir(String path, String fileName, T obj) {
+        File folder = new File(path);
+        folder.mkdirs();
+        path = path + fileName;
         try {
-            mapper.writeValue(new File(dir + File.separator + "user.json"), user);
+            mapper.writeValue(new File(path), obj);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static User readUser(String publicKeysPath) {
-        return null;
+    public <T> T readFromDir(String path, String fileName, Class<T> typeReference) {
+        File f = new File(path + fileName);
+        T obj = null;
+        try {
+            obj = mapper.readValue(f, typeReference);
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
+
+    public void writeUser(User user) {
+        String userFileName = user.getClass().getSimpleName() + ".json";
+        writeToDir(getUserDirectory(user), userFileName, user);
+    }
+
+    public User readUser(User user) {
+        String userFileName = user.getClass().getSimpleName() + ".json";
+        return readFromDir(getUserDirectory(user), userFileName, user.getClass());
+    }
+
+    public void writeAllPublicKeys(User user) {
+        for (Entry<String, Map<String, PublicKey>> auth : user.getAllPulicABEKeys().entrySet()) {
+            String path = getUserDirectory(user, "PublicABEKeys");
+            writeToDir(path, auth.getKey() + ".json", auth.getValue());
+            try {
+                Utility.writePublicKeys(path + auth.getKey(), auth.getValue());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
