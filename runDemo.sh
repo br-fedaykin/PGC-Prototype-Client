3b3569d8c485427f51d3bd9087f26a717e2edc51
Bruno Arruda <arrudabruno10@gmail.com>
2019-07-10 07:21:48 -0300

    Utility to migrate milestones commands from code

---
+++ runDemo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+case $1 in
+1) echo "Um" ;;
+2) echo "dois" ;;
+3) echo "tres" ;;
+4) echo "quatro" ;;
+5) echo "cinco" ;;
+6) echo "seis" ;;
+7) echo "sete" ;;
+8) echo "oito" ;;
+9) echo "nove" ;;
+10) echo "dez" ;;
+*) echo "Opcao Invalida!" ;;
+esac
