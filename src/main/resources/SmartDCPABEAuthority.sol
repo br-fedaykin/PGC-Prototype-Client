0bc0a3a75d716e1a4a819b1dc33be04002310125
Bruno Arruda <arrudabruno10@gmail.com>
2019-08-08 22:26:32 -0300

    change PublicKey and local variables structure
    Now PublicKey stores a byte array ranging from 97 to 127 bytes.
    Local bytes32 variables was joined in a array to avoid 'stack too deep'
    error.

--- src/main/resources/SmartDCPABEAuthority.sol
+++ src/main/resources/SmartDCPABEAuthority.sol
@@ -13,13 +13,16 @@ contract SmartDCPABEAuthority {
     struct PublicKey {
         uint64 id;
         bytes32 name;
-        Bytes122 eg1g1ai;
-        Bytes122 g1yi;
+        Bytes127 eg1g1ai;
+        Bytes127 g1yi;
     }
 
-    struct Bytes122 {
-        bytes32[3] chunks;
-        bytes26 lastChunk;
+    struct Bytes127 {
+        bytes32 chunk1;
+        bytes32 chunk2;
+        bytes32 chunk3;
+        bytes31 chunk4;
+        uint8 lastChunkSize;
     }
 
     address[] public certifierAddresses;
@@ -43,48 +46,47 @@ contract SmartDCPABEAuthority {
     }
 
     function addPublicKey(address addr, string memory name, bytes memory eg1g1ai, bytes memory g1yi) public {
-        bytes32 eg1g1aiChunk1;
-        bytes32 eg1g1aiChunk2;
-        bytes32 eg1g1aiChunk3;
-        bytes26 eg1g1aiChunk4;
-        bytes32 g1yiChunk1;
-        bytes32 g1yiChunk2;
-        bytes32 g1yiChunk3;
-        bytes26 g1yiChunk4;
+        bytes32 nameBytes32 = util.stringToBytes32(name);
+        bytes32[3] memory eg1g1aiChunks;
+        bytes31 eg1g1aiLastChunk;
+        uint8 eg1g1aiLastChunkSize = uint8(eg1g1ai.length % 32);
+        bytes32[3] memory g1yiChunks;
+        bytes31 g1yiLastChunk;
+        uint8 g1yiLastChunkSize = uint8(g1yi.length % 32);
 
         assembly {
-            eg1g1aiChunk1 := mload(add(eg1g1ai, 0x20))
-            eg1g1aiChunk2 := mload(add(eg1g1ai, 0x40))
-            eg1g1aiChunk3 := mload(add(eg1g1ai, 0x60))
-            eg1g1aiChunk4 := mload(add(eg1g1ai, 0x80))
-            g1yiChunk1 := mload(add(g1yi, 0x20))
-            g1yiChunk2 := mload(add(g1yi, 0x40))
-            g1yiChunk3 := mload(add(g1yi, 0x60))
-            g1yiChunk4 := mload(add(g1yi, 0x80))
+            // writing eg1g1ai byte dynamic array on the bytes32[3] structure
+            mstore(eg1g1aiChunks, mload(add(eg1g1ai, 0x20)))
+            mstore(add(eg1g1aiChunks, 0x20), mload(add(eg1g1ai, 0x40)))
+            mstore(add(eg1g1aiChunks, 0x40), mload(add(eg1g1ai, 0x60)))
+            eg1g1aiLastChunk := mload(add(eg1g1ai, 0x80))
+
+            // writing g1yi byte dynamic array on the bytes32[3] structure
+            mstore(g1yiChunks, mload(add(g1yi, 0x20)))
+            mstore(add(g1yiChunks, 0x20), mload(add(g1yi, 0x40)))
+            mstore(add(g1yiChunks, 0x40), mload(add(g1yi, 0x60)))
+            g1yiLastChunk := mload(add(g1yi, 0x80))
         }
 
-        addPublicKey(addr, util.stringToBytes32(name),
-                     eg1g1aiChunk1, eg1g1aiChunk2, eg1g1aiChunk3, eg1g1aiChunk4,
-                     g1yiChunk1, g1yiChunk2, g1yiChunk3, g1yiChunk4);
+        addPublicKey(addr, nameBytes32, eg1g1aiChunks, eg1g1aiLastChunk, eg1g1aiLastChunkSize,
+                     g1yiChunks, g1yiLastChunk, g1yiLastChunkSize);
     }
 
     function addPublicKey
     (
         address addr,
         bytes32 name,
-        bytes32 eg1g1aiChunk1,
-        bytes32 eg1g1aiChunk2,
-        bytes32 eg1g1aiChunk3,
-        bytes26 eg1g1aiChunk4,
-        bytes32 g1yiChunk1,
-        bytes32 g1yiChunk2,
-        bytes32 g1yiChunk3,
-        bytes26 g1yiChunk4
+        bytes32[3] memory eg1g1aiChunks,
+        bytes31 eg1g1aiLastChunk,
+        uint8 eg1g1aiLastChunkSize,
+        bytes32[3] memory g1yiChunks,
+        bytes31 g1yiLastChunk,
+        uint8 g1yiLastChunkSize
     )
         public
     {
-        Bytes122 memory eg1g1ai = Bytes122([eg1g1aiChunk1, eg1g1aiChunk2, eg1g1aiChunk3], eg1g1aiChunk4);
-        Bytes122 memory g1yi = Bytes122([g1yiChunk1, g1yiChunk2, g1yiChunk3], g1yiChunk4);
+        Bytes127 memory eg1g1ai = Bytes127(eg1g1aiChunks[0], eg1g1aiChunks[1], eg1g1aiChunks[2], eg1g1aiLastChunk, eg1g1aiLastChunkSize);
+        Bytes127 memory g1yi = Bytes127(g1yiChunks[0], g1yiChunks[1], g1yiChunks[2], g1yiLastChunk, g1yiLastChunkSize);
         Certifier storage c = certifiers[addr];
         c.keys[c.numPublicKeys] = PublicKey(c.numPublicKeys, name, eg1g1ai, g1yi);
         c.numPublicKeys++;
@@ -107,15 +109,15 @@ contract SmartDCPABEAuthority {
     function getPublicKeyByName
     (
         address addr,
-        bytes32 name
+        string memory name
     )
         public
         view
         returns
     (
-        bytes32 name_,
-        bytes memory eg1g1ai,
-        bytes memory g1yi
+        string memory,
+        bytes memory,
+        bytes memory
     )
     {
         int64 index = searchPublicKeyID(addr, name);
@@ -132,24 +134,24 @@ contract SmartDCPABEAuthority {
         view
         returns
     (
-        bytes32 name,
+        string memory name,
         bytes memory eg1g1ai,
         bytes memory g1yi
     )
     {
-        PublicKey storage key = certifiers[addr].keys[index];
+        PublicKey memory key = certifiers[addr].keys[index];
         return (
-            key.name,
+            util.bytes32ToString(key.name),
             abi.encodePacked(
-                key.eg1g1ai.chunks[0],
-                key.eg1g1ai.chunks[1],
-                key.eg1g1ai.chunks[2],
-                key.eg1g1ai.lastChunk),
+                key.eg1g1ai.chunk1,
+                key.eg1g1ai.chunk2,
+                key.eg1g1ai.chunk3,
+                util.trimBytes31(key.eg1g1ai.chunk4, key.eg1g1ai.lastChunkSize)),
             abi.encodePacked(
-                key.g1yi.chunks[0],
-                key.g1yi.chunks[1],
-                key.g1yi.chunks[2],
-                key.g1yi.lastChunk));
+                key.g1yi.chunk1,
+                key.g1yi.chunk2,
+                key.g1yi.chunk3,
+                util.trimBytes31(key.g1yi.chunk4, key.g1yi.lastChunkSize)));
     }
 
     function getCertifier
@@ -169,5 +171,4 @@ contract SmartDCPABEAuthority {
         Certifier storage c = certifiers[addr];
         return (addr, util.bytes32ToString(c.name), util.bytes32ToString(c.email), c.numPublicKeys);
     }
-
 }
