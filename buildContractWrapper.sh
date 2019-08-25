773357febba3920c600a8105758b0ee6466d3a33
Bruno Arruda <arrudabruno10@gmail.com>
2019-08-24 21:32:56 -0300

    changed contract wrapper classes generation folder

--- buildContractWrapper.sh
+++ buildContractWrapper.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-web3j solidity generate -a=src/main/resources/smartDCPABEFiles.abi -b=src/main/resources/smartDCPABEFiles.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=src/main/resources/smartDCPABEAuthority.abi -b=src/main/resources/smartDCPABEAuthority.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEFiles.abi -b=bin/src/main/resources/solidity/smartDCPABEFiles.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEAuthority.abi -b=bin/src/main/resources/solidity/smartDCPABEAuthority.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEKeys.abi -b=bin/src/main/resources/solidity/smartDCPABEKeys.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEUsers.abi -b=bin/src/main/resources/solidity/smartDCPABEUsers.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
