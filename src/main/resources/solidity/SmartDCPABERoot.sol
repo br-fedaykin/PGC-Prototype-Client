40c00b2ea01525ed6520c176bc3fe2718751e6ad
Bruno Arruda <arrudabruno10@gmail.com>
2019-09-09 17:23:57 -0300

    outline of deploying of smart contracts

--- src/main/resources/solidity/SmartDCPABERoot.sol
+++ src/main/resources/solidity/SmartDCPABERoot.sol
@@ -9,9 +9,28 @@ import "./SmartDCPABEAuthority.sol";
 
 contract SmartDCPABERoot {
 
-    enum contractTypes {AUTHORITY, FILES, KEYS, USERS, UTILITY }
+    enum contractType {AUTHORITY, FILES, KEYS, USERS, UTILITY }
+
+    mapping (uint => address) contractAddress;
+    SmartDCPABEAuthority authority;
+    SmartDCPABEFiles files;
+    SmartDCPABEKeys keys;
+    SmartDCPABEUsers users;
+    SmartDCPABEUtility util;
 
     // método para instanciar contratos não criados
+    function deployContracts() public {
+        util = new SmartDCPABEUtility();
+        contractAddress[uint(contractType.UTILITY)] = address(util);
+        users = new SmartDCPABEUsers();
+        contractAddress[uint(contractType.USERS)] = address(users);
+        files = new SmartDCPABEFiles(address(users));
+        contractAddress[uint(contractType.FILES)] = address(files);
+        authority = new SmartDCPABEAuthority(address(users));
+        contractAddress[uint(contractType.AUTHORITY)] = address(authority);
+        keys = new SmartDCPABEKeys(address(authority));
+        contractAddress[uint(contractType.KEYS)] = address(keys);
+    }
 
     // método para carregar contratos não criados
 
