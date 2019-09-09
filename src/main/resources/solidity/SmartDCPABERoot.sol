9e285a1b903d900097d07a80edbc235f0fef6a81
Bruno Arruda <arrudabruno10@gmail.com>
2019-09-08 22:10:06 -0300

    Creation of a contract to manage the others
    To allow circular references, a third contract will be created, that
    will create the contracts and allow update of those addresses.

---
+++ src/main/resources/solidity/SmartDCPABERoot.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.1;
+
+import "./SmartDCPABEAuthority.sol";
+import "./SmartDCPABEFiles.sol";
+import "./SmartDCPABEKeys.sol";
+import "./SmartDCPABEUsers.sol";
+import "./SmartDCPABEUtility.sol";
+import "./SmartDCPABEAuthority.sol";
+
+contract SmartDCPABERoot {
+
+    enum contractTypes {AUTHORITY, FILES, KEYS, USERS, UTILITY }
+
+    // método para instanciar contratos não criados
+
+    // método para carregar contratos não criados
+
+    // método para setar um contrato específico
+
+    // método para devolver todos os contratos
+
+    // método para atualizar as referências a um contrato
+
+}
