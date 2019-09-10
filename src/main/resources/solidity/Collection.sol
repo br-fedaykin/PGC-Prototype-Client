7bb1a35da3f0f598349a30bcd2905a299efb3b0d
Bruno Arruda <arrudabruno10@gmail.com>
2019-09-10 17:05:14 -0300

    defined setContractDependencies as interaface
    Moved enum ContractType from Root to Collection

--- src/main/resources/solidity/Collection.sol
+++ src/main/resources/solidity/Collection.sol
@@ -2,4 +2,12 @@ pragma solidity ^0.5.1;
 
 contract Collection {
 
+    address owner;
+    enum ContractType {AUTHORITY, FILES, KEYS, USERS, UTILITY }
+
+    constructor () public {
+        owner = msg.sender;
+    }
+
+    function setContractDependencies(Collection.ContractType contractType, address addr) public;
 }
