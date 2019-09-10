30c47aa6dfd868b471b776e867ed900a1e4a12be
Bruno Arruda <arrudabruno10@gmail.com>
2019-09-10 17:19:42 -0300

    Root sets contract from a address

--- src/main/resources/solidity/SmartDCPABERoot.sol
+++ src/main/resources/solidity/SmartDCPABERoot.sol
@@ -24,6 +24,27 @@ contract SmartDCPABERoot {
         }
     }
 
+    function setContractAddress(ContractType contractType, address addr) public returns (address) {
+        uint8 index = uint8(contractType);
+        require(index < 5, "targered contract type aren't implemented yet.");
+        if (contractType == ContractType.AUTHORITY) {
+            contracts[index] = SmartDCPABEAuthority(addr);
+            contractAddress[index] = addr;
+        } else if (contractType == ContractType.FILES) {
+            contracts[index] = SmartDCPABEFiles(addr);
+            contractAddress[index] = addr;
+        } else if (contractType == ContractType.KEYS) {
+            contracts[index] = SmartDCPABEKeys(addr);
+            contractAddress[index] = addr;
+        }else if (contractType == ContractType.USERS) {
+            contracts[index] = SmartDCPABEUsers(addr);
+            contractAddress[index] = addr;
+        } else if (contractType == ContractType.UTILITY) {
+            contracts[index] = SmartDCPABEUtility(addr);
+            contractAddress[index] = addr;
+        }
+    }
+
     function getContractAddress(ContractType contractType) public view returns (address) {
         return contractAddress[uint(contractType)];
     }
