b42e5b1759d38006b59c589fa3043657e0c15a30
Bruno Arruda <arrudabruno10@gmail.com>
2019-09-11 01:27:04 -0300

    Fix reference to ContractType and create alias

--- src/main/resources/solidity/SmartDCPABERoot.sol
+++ src/main/resources/solidity/SmartDCPABERoot.sol
@@ -12,47 +12,52 @@ contract SmartDCPABERoot {
 
     Collection[5] contracts;
     address[5] public contractAddress;
+    Collection.ContractType AUTHORITY = Collection.ContractType.AUTHORITY;
+    Collection.ContractType FILES = Collection.ContractType.FILES;
+    Collection.ContractType KEYS = Collection.ContractType.KEYS;
+    Collection.ContractType USERS = Collection.ContractType.USERS;
+    Collection.ContractType UTILITY = Collection.ContractType.UTILITY;
 
     function deployContracts() public {
-        contracts[uint(ContractType.AUTHORITY)] = new SmartDCPABEAuthority();
-        contracts[uint(ContractType.FILES)] = new SmartDCPABEFiles();
-        contracts[uint(ContractType.KEYS)] = new SmartDCPABEKeys();
-        contracts[uint(ContractType.USERS)] = new SmartDCPABEUsers();
-        contracts[uint(ContractType.UTILITY)] = new SmartDCPABEUtility();
+        contracts[uint(AUTHORITY)] = new SmartDCPABEAuthority();
+        contracts[uint(FILES)] = new SmartDCPABEFiles();
+        contracts[uint(KEYS)] = new SmartDCPABEKeys();
+        contracts[uint(USERS)] = new SmartDCPABEUsers();
+        contracts[uint(UTILITY)] = new SmartDCPABEUtility();
         for (uint8 i = 0; i < 5; i++) {
             contractAddress[i] = address(contracts[i]);
         }
     }
 
-    function setContractAddress(ContractType contractType, address addr) public returns (address) {
+    function setContractAddress(Collection.ContractType contractType, address addr) public {
         uint8 index = uint8(contractType);
         require(index < 5, "targered contract type aren't implemented yet.");
-        if (contractType == ContractType.AUTHORITY) {
+        if (contractType == AUTHORITY) {
             contracts[index] = SmartDCPABEAuthority(addr);
             contractAddress[index] = addr;
-        } else if (contractType == ContractType.FILES) {
+        } else if (contractType == FILES) {
             contracts[index] = SmartDCPABEFiles(addr);
             contractAddress[index] = addr;
-        } else if (contractType == ContractType.KEYS) {
+        } else if (contractType == KEYS) {
             contracts[index] = SmartDCPABEKeys(addr);
             contractAddress[index] = addr;
-        }else if (contractType == ContractType.USERS) {
+        }else if (contractType == USERS) {
             contracts[index] = SmartDCPABEUsers(addr);
             contractAddress[index] = addr;
-        } else if (contractType == ContractType.UTILITY) {
+        } else if (contractType == UTILITY) {
             contracts[index] = SmartDCPABEUtility(addr);
             contractAddress[index] = addr;
         }
     }
 
-    function setContractDependencies(ContractType contractType) public {
+    function setContractDependencies(Collection.ContractType contractType) public {
         uint8 index = uint8(contractType);
         require(index < 5, "targered contract type aren't implemented yet.");
         // if clauses to select contracts that will be sent to target contract
         // loop over selection to send a address over each iteration
     }
 
-    function getContractAddress(ContractType contractType) public view returns (address) {
+    function getContractAddress(Collection.ContractType contractType) public view returns (address) {
         return contractAddress[uint(contractType)];
     }
 }
