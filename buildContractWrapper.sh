53716ff4815232e0c6919c636c939dc86f3b75b4
Bruno Arruda <arrudabruno10@gmail.com>
2019-09-24 15:10:30 -0300

    Requests Contract added to wrapper builder script

--- buildContractWrapper.sh
+++ buildContractWrapper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEFiles.abi -b=bin/src/main/resources/solidity/smartDCPABEFiles.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
 web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEAuthority.abi -b=bin/src/main/resources/solidity/smartDCPABEAuthority.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEFiles.abi -b=bin/src/main/resources/solidity/smartDCPABEFiles.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABERequests.abi -b=bin/src/main/resources/solidity/smartDCPABERequests.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
 web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEKeys.abi -b=bin/src/main/resources/solidity/smartDCPABEKeys.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
 web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEUsers.abi -b=bin/src/main/resources/solidity/smartDCPABEUsers.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
