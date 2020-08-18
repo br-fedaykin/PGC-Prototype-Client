36001a8a2dff8ccf0aa612d6b2ebe54f92e898e0
Bruno Arruda <arrudabruno10@gmail.com>
2020-08-18 20:26:52 -0300

    Add header to stop background processes

--- runDemo.sh
+++ runDemo.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+
 echo "Initalizing Ganache..."
 
 cmd //c start ganache-cli -a 10 -d -m "pumpkin immense certain snack please patch universe leisure reopen truth eight gown" -p 7545
 
+sleep 3
 read -p 'Choose milestone to test: ' milestone
 
 case $milestone in
 1) echo "Um" ;;
 2) echo "dois" ;;
-3) echo "tres" ;;
+3) echo "três" ;;
 4) echo "quatro" ;;
 5) echo "cinco" ;;
 6) echo "seis" ;;
@@ -16,5 +21,5 @@ case $milestone in
 8) echo "oito" ;;
 9) echo "nove" ;;
 10) echo "dez" ;;
-*) echo "Opcao Invalida!" ;;
+*) echo "Opção Inválida!" ;;
 esac
