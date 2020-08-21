cbf6f4c36a971c4dc332c43df262a29640a71d36
Bruno Arruda <arrudabruno10@gmail.com>
2020-08-20 23:43:33 -0300

    option to compile solidity before wrapper generation
    option "--compile" in arguments will make solc compile the contracts to
    bin and abi formats.

--- buildContractWrapper.sh
+++ buildContractWrapper.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEAuthority.abi -b=bin/src/main/resources/solidity/smartDCPABEAuthority.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEFiles.abi -b=bin/src/main/resources/solidity/smartDCPABEFiles.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABERequests.abi -b=bin/src/main/resources/solidity/smartDCPABERequests.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEKeys.abi -b=bin/src/main/resources/solidity/smartDCPABEKeys.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=bin/src/main/resources/solidity/smartDCPABEUsers.abi -b=bin/src/main/resources/solidity/smartDCPABEUsers.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+if [[ "$1" = "--compile" ]]; then
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEAuthority.sol
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEFiles.sol
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEKeys.sol
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABERequests.sol
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABERoot.sol
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEUsers.sol
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEUtility.sol
+fi
+
+web3j solidity generate -a=bin/src/solidity/smartDCPABEAuthority.abi -b=bin/src/solidity/smartDCPABEAuthority.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/solidity/smartDCPABEFiles.abi -b=bin/src/solidity/smartDCPABEFiles.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/solidity/smartDCPABEKeys.abi -b=bin/src/solidity/smartDCPABEKeys.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/solidity/smartDCPABERequests.abi -b=bin/src/solidity/smartDCPABERequests.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/solidity/smartDCPABERoot.abi -b=bin/src/solidity/smartDCPABERoot.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/solidity/smartDCPABEUsers.abi -b=bin/src/solidity/smartDCPABEUsers.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+web3j solidity generate -a=bin/src/solidity/smartDCPABEUtility.abi -b=bin/src/solidity/smartDCPABEUtility.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
