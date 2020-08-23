62212d9233c637bdc28102894bb2acf896615b8b
Bruno Arruda <arrudabruno10@gmail.com>
2020-08-23 19:20:33 -0300

    Improviment on script output

--- buildContractWrapper.sh
+++ buildContractWrapper.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+echo ""
 if [[ "$1" = "--compile" ]]; then
-    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEAuthority.sol
-    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEFiles.sol
-    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEKeys.sol
-    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABERequests.sol
-    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABERoot.sol
-    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEUsers.sol
-    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEUtility.sol
+    echo -n "Compiling solidity contracts into EVM Binary code and ABI ... "
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEAuthority.sol > logs/solc_build.log
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEFiles.sol >> logs/solc_build.log
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEKeys.sol >> logs/solc_build.log
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABERequests.sol >> logs/solc_build.log
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABERoot.sol >> logs/solc_build.log
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEUsers.sol >> logs/solc_build.log
+    solc -o bin/src/solidity --bin --abi --optimize --overwrite --gas --evm-version petersburg src/main/resources/solidity/smartDCPABEUtility.sol >> logs/solc_build.log
+    echo "Done, files are in bin/src/solidity"
 fi
 
-web3j solidity generate -a=bin/src/solidity/smartDCPABEAuthority.abi -b=bin/src/solidity/smartDCPABEAuthority.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=bin/src/solidity/smartDCPABEFiles.abi -b=bin/src/solidity/smartDCPABEFiles.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=bin/src/solidity/smartDCPABEKeys.abi -b=bin/src/solidity/smartDCPABEKeys.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=bin/src/solidity/smartDCPABERequests.abi -b=bin/src/solidity/smartDCPABERequests.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=bin/src/solidity/smartDCPABERoot.abi -b=bin/src/solidity/smartDCPABERoot.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=bin/src/solidity/smartDCPABEUsers.abi -b=bin/src/solidity/smartDCPABEUsers.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
-web3j solidity generate -a=bin/src/solidity/smartDCPABEUtility.abi -b=bin/src/solidity/smartDCPABEUtility.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain
+echo ""
+echo -n "Generating Java wrapper classes for each contract ... "
+web3j solidity generate -a=bin/src/solidity/smartDCPABEAuthority.abi -b=bin/src/solidity/smartDCPABEAuthority.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain > /dev/null
+web3j solidity generate -a=bin/src/solidity/smartDCPABEFiles.abi -b=bin/src/solidity/smartDCPABEFiles.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain > /dev/null
+web3j solidity generate -a=bin/src/solidity/smartDCPABEKeys.abi -b=bin/src/solidity/smartDCPABEKeys.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain > /dev/null
+web3j solidity generate -a=bin/src/solidity/smartDCPABERequests.abi -b=bin/src/solidity/smartDCPABERequests.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain > /dev/null
+web3j solidity generate -a=bin/src/solidity/smartDCPABERoot.abi -b=bin/src/solidity/smartDCPABERoot.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain > /dev/null
+web3j solidity generate -a=bin/src/solidity/smartDCPABEUsers.abi -b=bin/src/solidity/smartDCPABEUsers.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain > /dev/null
+web3j solidity generate -a=bin/src/solidity/smartDCPABEUtility.abi -b=bin/src/solidity/smartDCPABEUtility.bin -o=src/main/java -p=com.brunoarruda.hyperdcpabe.blockchain > /dev/null
+echo "Done, classes are inside blockchain package."
