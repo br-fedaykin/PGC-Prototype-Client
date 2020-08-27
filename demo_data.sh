93af8e0b6a94f0a461dbe39c8e05cc8d23d28d59
Bruno Arruda <arrudabruno10@gmail.com>
2020-08-26 23:10:57 -0300

    milestone 1 avaiable in demonstration script

---
+++ demo_data.sh
@@ -0,0 +1,28 @@
+declare -A admin
+declare -A crm
+declare -A alice
+declare -A bob
+
+admin[name]="admin"
+admin[email]="admin@email.com"
+admin[address]="0xFae373E0BFfaE794fA818D749D6da38D4f7cA986"
+admin[gid]="admin-0xFae373E0BFfaE794fA818D749D6da38D4f7cA986"
+admin[privateKey]="e4d8c81796894ea5bf202e3a3204948dddd62f4d709c278bf8096898957be241"
+
+crm[name]="CRM"
+crm[email]="crm@email.com"
+crm[address]="0xFB7EAfB7fBdaA775d0D52fAaEBC525C1cE173EE0"
+crm[gid]="CRM-0xFB7EAfB7fBdaA775d0D52fAaEBC525C1cE173EE0"
+crm[privateKey]="e15b910f8c61580befebecff2d79abf38998035cbc317400a96c4736a424f6dc"
+
+alice[name]="Alice"
+alice[email]="alice@email.com"
+alice[address]="0xb038476875480BCE0D0FCf0991B4BB108A3FCB47"
+alice[gid]="Alice-0xb038476875480BCE0D0FCf0991B4BB108A3FCB47"
+alice[privateKey]="4237a475aa6579f2a0fc85d90cbcda1fad3db70391315a6c37b51de3a8cb503a"
+
+bob[name]="Bob"
+bob[email]="bob@email.com"
+bob[address]="0xF7908374b1a445cCf65F729887dbB695c918BEfc"
+bob[gid]="Bob-0xF7908374b1a445cCf65F729887dbB695c918BEfc"
+bob[privateKey]="ab0439882857ffb5859c1a3a6bf40a6848daeaab6605c873c3e425de53c2c4ab"
--- runDemo.sh
+++ runDemo.sh
@@ -1,7 +1,10 @@
+data=./demo_data.sh
+
 #!/bin/bash
 
-trap "exit" INT TERM ERR
-trap "kill 0" EXIT
+# this should kill ganache-cli after calling exit, but it didn't work. Idk why
+# trap "exit" INT TERM ERR
+# trap "kill 0" EXIT
 
 # command and option to call ganache-cli without blocking the shell in linux environments
 shell=sh
@@ -14,22 +17,64 @@ if [[ "$OSTYPE" = "msys" ]]; then
 fi
 
 echo "Initalizing Ganache..."
-
 $shell $option ganache-cli -a 10 -d -m "pumpkin immense certain snack please patch universe leisure reopen truth eight gown" -p 7545
 
 sleep 3
 read -p 'Choose milestone to test: ' milestone
 
+source $data
+
 case $milestone in
-1) echo "Um" ;;
-2) echo "dois" ;;
-3) echo "três" ;;
-4) echo "quatro" ;;
-5) echo "cinco" ;;
-6) echo "seis" ;;
-7) echo "sete" ;;
-8) echo "oito" ;;
-9) echo "nove" ;;
-10) echo "dez" ;;
-*) echo "Opção Inválida!" ;;
+    1) echo "mileston 1"
+
+    # admin inicia o sistema e cria os contratos
+    java -jar $1 --init http://127.0.0.1:7545 ${admin[name]} ${admin[email]} ${admin[privateKey]}
+
+    # certificador cria perfil e atributo, e publica ambos
+    java -jar $1 --create-user ${crm[name]} ${crm[email]} ${crm[privateKey]}
+    java -jar $1 --create-certifier
+    java -jar $1 --create-attributes atributo1 atributo2 atributo3
+    java -jar $1 --publish user certifier attributes
+
+    # usuário 1 - Bob, cria perfil e solicita concessão do atributo 1 (chave pessoal ABE)
+    java -jar $1 --create-user ${bob[name]} ${bob[email]} ${bob[privateKey]}
+    java -jar $1 --publish user
+    java -jar $1 --request-attribute ${crm[gid]} atributo1
+
+    # usuário 2 - Alice, cria perfil, recebe chaves públicas e criptografa um documento
+    java -jar $1 --create-user ${alice[name]} ${alice[email]} ${alice[privateKey]}
+    java -jar $1 --publish user
+    java -jar $1 --get-attributes ${crm[gid]} atributo1
+
+    echo "put a file inside Alice folder for encryption"
+    sleep 5
+
+    read -p "write file name in Alice folder for encryption: " filename
+    java -jar $1 --encrypt $filename atributo1 ${crm[gid]}
+    java -jar $1 --send $filename
+
+    # certificador recebe requisição de atributo e o concede ao Bob
+    java -jar $1 --load ${crm[gid]}
+    java -jar $1 --check-requests pending
+    java -jar $1 --yield-attributes ${bob[gid]} 0
+    java -jar $1 --send attributes ${bob[gid]}
+
+    # usuário 1 - Bob, de posse do atributo, o descriptografa
+    java -jar $1 --load ${bob[gid]}
+    java -jar $1 --check-requests ok
+    java -jar $1 --check-requests download
+    java -jar $1 --get-recordings ${alice[gid]} $filename
+    java -jar $1 --decrypt $filename ;;
+    2) echo "dois" ;;
+    3) echo "três" ;;
+    4) echo "quatro" ;;
+    5) echo "cinco" ;;
+    6) echo "seis" ;;
+    7) echo "sete" ;;
+    8) echo "oito" ;;
+    9) echo "nove" ;;
+    10) echo "dez" ;;
+    *) echo "Opção Inválida!" ;;
 esac
+
+exit
