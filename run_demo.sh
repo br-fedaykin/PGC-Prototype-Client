ab84422fe3fc36a5ddb993c8dc98342a9ef9ee03
Bruno Arruda <arrudabruno10@gmail.com>
2020-09-21 19:51:46 -0300

    hide warning message from illegal access to security.provider

--- run_demo.sh
+++ run_demo.sh
@@ -2,13 +2,16 @@ data=./demo_data.sh
 
 #!/bin/bash
 
-# this should kill ganache-cli after calling exit, but it didn't work. Idk why
+# this should kill ganache-cli after calling exit, but it didn't work and Idk why
 # trap "exit" INT TERM ERR
 # trap "kill 0" EXIT
 
 # command and option to call ganache-cli without blocking the shell in linux environments
 shell=sh
 option=-c
+
+# this is necessary until DCPABE library updates org.bouncycastle version
+java_command="java --add-opens java.base/sun.security.provider=ALL-UNNAMED -jar"
 
 # command and option to call ganache-cli without blocking the shell in Windows environment
 if [[ "$OSTYPE" = "msys" ]]; then
@@ -25,46 +28,46 @@ read -p 'Choose milestone to test: ' milestone
 source $data
 
 case $milestone in
-    1) echo "mileston 1"
+    1) echo "milestone 1"
 
     # admin inicia o sistema e cria os contratos
-    java -jar $1 --init http://127.0.0.1:7545 ${admin[name]} ${admin[email]} ${admin[privateKey]}
+    $java_command $1 --init http://127.0.0.1:7545 ${admin[name]} ${admin[email]} ${admin[privateKey]}
 
     # certificador cria perfil e atributo, e publica ambos
-    java -jar $1 --create-user ${crm[name]} ${crm[email]} ${crm[privateKey]}
-    java -jar $1 --create-certifier
-    java -jar $1 --create-attributes atributo1 atributo2 atributo3
-    java -jar $1 --publish user certifier attributes
+    $java_command $1 --create-user ${crm[name]} ${crm[email]} ${crm[privateKey]}
+    $java_command $1 --create-certifier
+    $java_command $1 --create-attributes atributo1 atributo2 atributo3
+    $java_command $1 --publish user certifier attributes
 
     # usuário 1 - Bob, cria perfil e solicita concessão do atributo 1 (chave pessoal ABE)
-    java -jar $1 --create-user ${bob[name]} ${bob[email]} ${bob[privateKey]}
-    java -jar $1 --publish user
-    java -jar $1 --request-attribute ${crm[gid]} atributo1
+    $java_command $1 --create-user ${bob[name]} ${bob[email]} ${bob[privateKey]}
+    $java_command $1 --publish user
+    $java_command $1 --request-attribute ${crm[gid]} atributo1
 
     # usuário 2 - Alice, cria perfil, recebe chaves públicas e criptografa um documento
-    java -jar $1 --create-user ${alice[name]} ${alice[email]} ${alice[privateKey]}
-    java -jar $1 --publish user
-    java -jar $1 --get-attributes ${crm[gid]} atributo1
+    $java_command $1 --create-user ${alice[name]} ${alice[email]} ${alice[privateKey]}
+    $java_command $1 --publish user
+    $java_command $1 --get-attributes ${crm[gid]} atributo1
 
     echo "put a file inside Alice folder for encryption"
     sleep 5
 
     read -p "write file name in Alice folder for encryption: " filename
-    java -jar $1 --encrypt $filename atributo1 ${crm[gid]}
-    java -jar $1 --send $filename
+    $java_command $1 --encrypt $filename atributo1 ${crm[gid]}
+    $java_command $1 --send $filename
 
     # certificador recebe requisição de atributo e o concede ao Bob
-    java -jar $1 --load ${crm[gid]}
-    java -jar $1 --check-requests pending
-    java -jar $1 --yield-attributes ${bob[gid]} 0
-    java -jar $1 --send attributes ${bob[gid]}
+    $java_command $1 --load ${crm[gid]}
+    $java_command $1 --check-requests pending
+    $java_command $1 --yield-attributes ${bob[gid]} 0
+    $java_command $1 --send attributes ${bob[gid]}
 
     # usuário 1 - Bob, de posse do atributo, o descriptografa
-    java -jar $1 --load ${bob[gid]}
-    java -jar $1 --check-requests ok
-    java -jar $1 --check-requests download
-    java -jar $1 --get-recordings ${alice[gid]} $filename
-    java -jar $1 --decrypt $filename ;;
+    $java_command $1 --load ${bob[gid]}
+    $java_command $1 --check-requests ok
+    $java_command $1 --check-requests download
+    $java_command $1 --get-recordings ${alice[gid]} $filename
+    $java_command $1 --decrypt $filename ;;
     2) echo "dois" ;;
     3) echo "três" ;;
     4) echo "quatro" ;;
