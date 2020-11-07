40b37f3ecdde08f161fc8f8f76a1744c3bc6b2d3
Bruno Arruda <arrudabruno10@gmail.com>
2020-11-07 15:36:40 -0300

    change field types in Authority Smart Contract

--- src/main/solidity/SmartDCPABEAuthority.sol
+++ src/main/solidity/SmartDCPABEAuthority.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.5.1;
 
-import "./SmartDCPABEUtility.sol";
 import "./SmartDCPABEUsers.sol";
 import "./Collection.sol";
 
@@ -8,23 +7,20 @@ contract SmartDCPABEAuthority is Collection {
 
     struct Certifier {
         address addr;
-        bytes32 name;
-        bytes32 email;
+        string name;
+        string email;
         uint64 numPublicKeys;
     }
 
     address[] public certifierAddresses;
     mapping (address => Certifier) certifiers;
-    SmartDCPABEUtility util;
     SmartDCPABEUsers user;
     address contractKeys;
 
     constructor(address root) Collection(root) public {}
 
     function setContractDependencies(ContractType contractType, address addr) public onlyOwner {
-        if (contractType == ContractType.UTILITY) {
-            util = SmartDCPABEUtility(addr);
-        } else if (contractType == ContractType.USERS) {
+        if (contractType == ContractType.USERS) {
             user = SmartDCPABEUsers(addr);
         } else if (contractType == ContractType.KEYS) {
             contractKeys = addr;
@@ -37,12 +33,12 @@ contract SmartDCPABEAuthority is Collection {
     }
 
     function isCertifier(address addr) public view returns (bool) {
-        return certifiers[addr].name != bytes32(0);
+        return (bytes(certifiers[addr].name)).length > 0;
     }
 
     function addCertifier(address addr, string memory name, string memory email) public {
         certifierAddresses.push(addr);
-        certifiers[addr] = Certifier(addr, util.stringToBytes32(name), util.stringToBytes32(email), 0);
+        certifiers[addr] = Certifier(addr, name, email, 0);
     }
 
     function getCertifier
@@ -60,6 +56,6 @@ contract SmartDCPABEAuthority is Collection {
     )
     {
         Certifier storage c = certifiers[addr];
-        return (c.addr, util.bytes32ToString(c.name), util.bytes32ToString(c.email), c.numPublicKeys);
+        return (c.addr, c.name, c.email, c.numPublicKeys);
     }
 }
